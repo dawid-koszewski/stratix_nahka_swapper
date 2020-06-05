1ee9a1d0dc48b91e813c5c7584ee7eea539f28bf
Dawid <dawid@dawid.pl>
2020-06-05 16:03:15 +0200

    01b (2019.10.31)

--- stratix_nahka_swapper.py
+++ stratix_nahka_swapper.py
@@ -2,148 +2,123 @@
 
 #-----------------------------------------------------------------
 # author:   dawid.koszewski@nokia.com
-# date:     2019.10.28
+# date:     2019.10.30
 #-----------------------------------------------------------------
 
 #-----------------------------------------------------------------
 # set absolute path to your zutil or pass it as second parameter to this script
 #-----------------------------------------------------------------
-path_to_zutil = 'C:\zutil\zutil.exe'
+#path_to_zutil = 'C:\zutil\zutil.exe'
 
-pathToStratix = 'https://artifactory-espoo1.int.net.nokia.com/artifactory/mnprf_brft-local/mMIMO_FDD/FB1813_Z/PROD_mMIMO_FDD_FB1813_Z_release/1578/C_Element/SE_RFM/SS_mMIMO_FDD/Target/SRM-rfsw-image-install_z-uber-0x121E7D81.tar'
+#pathToStratix = 'https://artifactory-espoo1.int.net.nokia.com/artifactory/mnprf_brft-local/mMIMO_FDD/FB1813_Z/PROD_mMIMO_FDD_FB1813_Z_release/1578/C_Element/SE_RFM/SS_mMIMO_FDD/Target/SRM-rfsw-image-install_z-uber-0x121E7D81.tar'
 
 import sys
 import re
 import os
 import time
 import subprocess
-#import glob
 import shutil
+import wget
+import tarfile
 
-pathToNahka = 'V:\\dkoszewski\\rel5_mmimo\\nahka\\tmp\\deploy\\images\\nahka\\'
-#dodac ifa ktory dodaje lub odejmuje ostatni backslash
-pathToNahka2 = r'V:\dkoszewski\rel5_mmimo\nahka\tmp\deploy\images\nahka\\'
+pathToNahka = ""
+pathToStratix = ""
 
-pathToNahka2norm = os.path.abspath(pathToNahka2)
-pathToNahka2norm2 = os.path.dirname(pathToNahka2)
+with open('.\tp_generator_stratix_nahka.ini', 'r') as f:
+    for line in f:
+        if re.search(r'(deploy)[\/\\](images)[\/\\](nahka)', line):
+            pathToNahka = line.strip()
+            print(pathToNahka)
+        else:
+            pathToStratix = line.strip()
+            print(pathToStratix)
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
 
-print(pathToNahka)
-print(pathToNahka2)
-print(pathToNahka2norm)
-print(pathToNahka2norm2)
 
-
-#os.path.join
-
-
-# print(pathToNahka2norm)
-
-# files = []
-# for file in glob.glob(pathToNahka2norm + '\FRM-rfsw-image-install_*-multi.tar'):
-    # files.append(file)
-    # print(file)
-
-    #print(files[:-1])
-
-dirlist = os.listdir(pathToNahka2)
-
-#print(dirlist)
-
-#nahkaFilesDictionary = {}
+listDirNahka = os.listdir(pathToNahka)
 nahkaFilesList = []
 
-for item in dirlist:
-    #print(os.path.dirname(item))
-    if os.path.isfile(pathToNahka2norm +'\\' + item):
-        #print(item)
-        #if re.search(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', item):
+for item in listDirNahka:
+    if os.path.isfile(os.path.join(pathToNahka, item)):
         if re.match(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', item):
-            #print(item)
-            #nahkaFilesDictionary[re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', r'\2', item)] = item
             nahkaFilesList.append(item)
-
-# nahkaFilesDictionary['20191024132842'] = 'FRM-rfsw-image-install_20191024132842-multi.tar'
-# nahkaFilesDictionary['20191024132841'] = 'FRM-rfsw-image-install_20191024132841-multi.tar'
-nahkaFilesList.append('FRM-rfsw-image-install_20191024132842-multi.tar')
-nahkaFilesList.append('FRM-rfsw-image-install_20191024132841-multi.tar')
-
-#print(nahkaFilesDictionary)
-#print(sorted(nahkaFilesDictionary))
-
-#nahkaFileLast = nahkaFilesDictionary.get(sorted(nahkaFilesDictionary)[-1])
-#print(nahkaFileLast)
-
 
 def getDateFromFilename(filename):
     return re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', r'\2', filename)
 
-#print(nahkaFilesList)
 nahkaFilesList.sort(key = getDateFromFilename, reverse = False)
-#print(nahkaFilesList)
-
-
-nahkaFileLast2 = nahkaFilesList[-1]
-print(nahkaFileLast2)
-
-nahkaFilePath = pathToNahka2norm +'\\' + nahkaFileLast2
-print(nahkaFilePath)
-#----------------------------------------------------------------------------------------------------
+nahkaFileLast = nahkaFilesList[-1]
+nahkaFilePath = os.path.join(pathToNahka, nahkaFileLast)
 #----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
 
 
-shutil.copy2(nahkaFilePath, '.\\')#, *, follow_symlinks = True)
+listDirStratix = os.listdir(pathToStratix)
+stratixFileLast = ""
 
-if re.search(r'https://', pathToStratix):
-    import wget
-    #wget.download(pathToStratix, './')
-else:
-    shutil.copy2(pathToStratix, './')#, *, follow_symlinks = True)
+for item in listDirStratix:
+    if os.path.isfile(os.path.join(pathToStratix, item)):
+        if re.match(r'(SRM-rfsw-image-install_z-uber-0x)([a-fA-F0-9]{8})(.tar)', item):
+            stratixFileLast = item
 
-# dodac sprawdzanie czy plik juz istnieje lokalnie
+stratixFilePath = os.path.join(pathToStratix, stratixFileLast)
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
 
 
+shutil.copy2(nahkaFilePath, '.')#, *, follow_symlinks = True)
+
+# if re.search(r'https://', pathToStratix):
+    # wget.download(pathToStratix, '.')
+# else:
+    # shutil.copy2(stratixFilePath, '.')#, *, follow_symlinks = True)
+
+shutil.copy2(stratixFilePath, '.')#, *, follow_symlinks = True)
+
+
+# dodac sprawdzanie czy plik juz istnieje lokalnie i wtedy sciagac
 
 
 #----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------
 
-import tarfile
-with tarfile.open('SRM-rfsw-image-install_z-uber-0x121E7D81.tar', 'r') as tar:
-    tar.extractall(path= '.\SRM_temp')
+pathToTemp = r'.\SRM_temp'
+
+with tarfile.open(stratixFileLast, 'r') as tar:
+    tar.extractall(path = pathToTemp)
 
 
-dirlist = os.listdir('.\SRM_temp')
+listDirTemp = os.listdir(pathToTemp)
 
-for filename in dirlist:
-    if os.path.isfile('.\SRM_temp\\' + filename):
-        if re.match(r'.*-installer.sh', filename):
-            with open('.\SRM_temp\\' + filename, 'r') as f:
+for tempFile in listDirTemp:
+    tempFilePath = os.path.join(pathToTemp, tempFile)
+    if os.path.isfile(tempFilePath):
+        if re.match(r'.*-installer.sh', tempFile):
+            with open(tempFilePath, 'r') as f:
                 file_content = f.read()
-                file_content = re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', nahkaFileLast2, file_content)
-            with open('.\SRM_temp\\' + filename, 'w') as f:
+                file_content = re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', nahkaFileLast, file_content)
+            with open(tempFilePath, 'w') as f:
                 f.write(file_content)
 
+pathToTempArtifacts = r'.\SRM_temp\artifacts'
 
-dirlist2 = os.listdir(r'.\SRM_temp\artifacts\\')
-for filename in dirlist2:
-    print(filename)
-    if re.match(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', r'.\SRM_temp\artifacts\\' + filename):
+listDirTempArtifacts = os.listdir(pathToTempArtifacts)
+
+for tempFileArtifacts in listDirTempArtifacts:
+    if re.match(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', tempFileArtifacts):
         try:
-            os.remove(r'.\SRM_temp\artifacts\\' + filename)
+            os.remove(os.path.join(pathToTempArtifacts, tempFileArtifacts))
         except OSError:
             pass
 
+shutil.copy(nahkaFileLast, pathToTempArtifacts)
 
-shutil.copy(nahkaFileLast2, r'.\SRM_temp\artifacts\\')
-
-
-# podmienic jeszcze obraz nahki w artifacts
 
 
 with tarfile.open('SRM-rfsw-image-install_z-uber-0x00000000.tar', 'w') as tar:
-    for item in dirlist:
-        tar.add('.\SRM_temp\\' + item, arcname = item)
+    for item in listDirTemp:
+        tar.add(os.path.join(pathToTemp, item), arcname = item)
 
 
