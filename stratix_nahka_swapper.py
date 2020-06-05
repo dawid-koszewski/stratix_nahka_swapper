58888e929e65feac7b1fe95d16959690ae23aa0b
Dawid <dawid@dawid.pl>
2020-06-05 16:04:04 +0200

    01c (2019.10.31)

--- stratix_nahka_swapper.py
+++ stratix_nahka_swapper.py
@@ -5,13 +5,6 @@
 # date:     2019.10.30
 #-----------------------------------------------------------------
 
-#-----------------------------------------------------------------
-# set absolute path to your zutil or pass it as second parameter to this script
-#-----------------------------------------------------------------
-#path_to_zutil = 'C:\zutil\zutil.exe'
-
-#pathToStratix = 'https://artifactory-espoo1.int.net.nokia.com/artifactory/mnprf_brft-local/mMIMO_FDD/FB1813_Z/PROD_mMIMO_FDD_FB1813_Z_release/1578/C_Element/SE_RFM/SS_mMIMO_FDD/Target/SRM-rfsw-image-install_z-uber-0x121E7D81.tar'
-
 import sys
 import re
 import os
@@ -21,12 +14,22 @@ import shutil
 import wget
 import tarfile
 
+pathToIniFile = r'.\tp_generator_stratix_nahka.ini'
 pathToNahka = ""
 pathToStratix = ""
+pathToTemp = r'.\SRM_temp'
+pathToTempArtifacts = r'.\SRM_temp\artifacts'
+stratixFileNameNew = r'.\SRM-rfsw-image-install_z-uber-0x00000000.tar'
 
-with open('.\tp_generator_stratix_nahka.ini', 'r') as f:
+pathMatcherNahka = r'(deploy)[\/\\](images)[\/\\](nahka)'
+imageMatcherNahka = r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)'
+imageMatcherStratix = r'.*(SRM-rfsw-image-install_z-uber-0x)([a-fA-F0-9]{8})(.tar)'
+installerMatcher = r'.*-installer.sh'
+
+
+with open(pathToIniFile, 'r') as f:
     for line in f:
-        if re.search(r'(deploy)[\/\\](images)[\/\\](nahka)', line):
+        if re.search(pathMatcherNahka, line):
             pathToNahka = line.strip()
             print(pathToNahka)
         else:
@@ -36,89 +39,114 @@ with open('.\tp_generator_stratix_nahka.ini', 'r') as f:
 #----------------------------------------------------------------------------------------------------
 
 
-listDirNahka = os.listdir(pathToNahka)
-nahkaFilesList = []
+def getDateFromNahkaFile(filename):
+    return re.sub(imageMatcherNahka, r'\2', filename)
 
-for item in listDirNahka:
-    if os.path.isfile(os.path.join(pathToNahka, item)):
-        if re.match(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', item):
-            nahkaFilesList.append(item)
+def getLastModificationTime(filename):
+    return os.path.getmtime(filename)
 
-def getDateFromFilename(filename):
-    return re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', r'\2', filename)
-
-nahkaFilesList.sort(key = getDateFromFilename, reverse = False)
-nahkaFileLast = nahkaFilesList[-1]
-nahkaFilePath = os.path.join(pathToNahka, nahkaFileLast)
-#----------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------
-
-
-listDirStratix = os.listdir(pathToStratix)
-stratixFileLast = ""
-
-for item in listDirStratix:
-    if os.path.isfile(os.path.join(pathToStratix, item)):
-        if re.match(r'(SRM-rfsw-image-install_z-uber-0x)([a-fA-F0-9]{8})(.tar)', item):
-            stratixFileLast = item
-
-stratixFilePath = os.path.join(pathToStratix, stratixFileLast)
-#----------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------
+def getLastFileFromDir(pathToDir, matcher, comparator):
+    filesList = []
+    listDir = os.listdir(pathToDir)
+    for item in listDir:
+        filePath = os.path.join(pathToDir, item)
+        if os.path.isfile(filePath):
+            if re.match(matcher, item):
+                filesList.append(filePath)
+    filesList.sort(key = comparator, reverse = False)
+    return filesList[-1]
 
 
-shutil.copy2(nahkaFilePath, '.')#, *, follow_symlinks = True)
-
-# if re.search(r'https://', pathToStratix):
-    # wget.download(pathToStratix, '.')
-# else:
-    # shutil.copy2(stratixFilePath, '.')#, *, follow_symlinks = True)
-
-shutil.copy2(stratixFilePath, '.')#, *, follow_symlinks = True)
+def getStratixFileNameFromLink(imageMatcherStratix, pathToStratix):
+    return re.sub(imageMatcherStratix, r'\1\2\3', pathToStratix)
 
 
-# dodac sprawdzanie czy plik juz istnieje lokalnie i wtedy sciagac
+def getImageFromLinsee(fileName, filePath):
+    if not os.path.isfile(fileName):
+        shutil.copy2(filePath, '.')#, *, follow_symlinks = True)
 
 
-#----------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------------------
-
-pathToTemp = r'.\SRM_temp'
-
-with tarfile.open(stratixFileLast, 'r') as tar:
-    tar.extractall(path = pathToTemp)
+def getImageFromArtifactory(fileName, filePath):
+    if not os.path.isfile(fileName):
+        wget.download(pathToStratix, '.')
 
 
-listDirTemp = os.listdir(pathToTemp)
+def extractStratixImage(stratixFileName, pathToTemp):
+    with tarfile.open(stratixFileName, 'r') as tar:
+        tar.extractall(path = pathToTemp)
 
-for tempFile in listDirTemp:
-    tempFilePath = os.path.join(pathToTemp, tempFile)
-    if os.path.isfile(tempFilePath):
-        if re.match(r'.*-installer.sh', tempFile):
-            with open(tempFilePath, 'r') as f:
-                file_content = f.read()
-                file_content = re.sub(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', nahkaFileLast, file_content)
-            with open(tempFilePath, 'w') as f:
-                f.write(file_content)
 
-pathToTempArtifacts = r'.\SRM_temp\artifacts'
+def setNewNahkaFileNameInInstallerScripts(pathToTemp, installerMatcher):
+    listDirTemp = os.listdir(pathToTemp)
+    for tempFile in listDirTemp:
+        tempFilePath = os.path.join(pathToTemp, tempFile)
+        if os.path.isfile(tempFilePath):
+            if re.match(installerMatcher, tempFile):
+                with open(tempFilePath, 'r') as f:
+                    file_content = f.read()
+                    file_content = re.sub(imageMatcherNahka, nahkaFileName, file_content)
+                with open(tempFilePath, 'w') as f:
+                    f.write(file_content)
 
-listDirTempArtifacts = os.listdir(pathToTempArtifacts)
 
-for tempFileArtifacts in listDirTempArtifacts:
-    if re.match(r'(FRM-rfsw-image-install_)([0-9]{14})(-multi.tar)', tempFileArtifacts):
-        try:
-            os.remove(os.path.join(pathToTempArtifacts, tempFileArtifacts))
-        except OSError:
-            pass
+def removeOldNahkaImageFile(pathToTempArtifacts, imageMatcherNahka):
+    listDirTempArtifacts = os.listdir(pathToTempArtifacts)
+    for tempFileArtifacts in listDirTempArtifacts:
+        if re.match(imageMatcherNahka, tempFileArtifacts):
+            try:
+                os.remove(os.path.join(pathToTempArtifacts, tempFileArtifacts))
+            except OSError as e:
+                print("Error: %s - %s." % (e.filename, e.strerror))
 
-shutil.copy(nahkaFileLast, pathToTempArtifacts)
+
+def copyNewNahkaImageFileToArtifacts(nahkaFileName, pathToTempArtifacts):
+    shutil.copy(nahkaFileName, pathToTempArtifacts)
+
+
+def createNewStratixImage(pathToTemp, stratixFileNameNew):
+    listDirTemp = os.listdir(pathToTemp)
+    with tarfile.open(stratixFileNameNew, 'w') as tar:
+        for item in listDirTemp:
+            tar.add(os.path.join(pathToTemp, item), arcname = item)
+
+
+def removeTempDirectory(pathToTemp):
+    try:
+        shutil.rmtree(pathToTemp)
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
 
 
 
-with tarfile.open('SRM-rfsw-image-install_z-uber-0x00000000.tar', 'w') as tar:
-    for item in listDirTemp:
-        tar.add(os.path.join(pathToTemp, item), arcname = item)
+nahkaFilePath = getLastFileFromDir(pathToNahka, imageMatcherNahka, getLastModificationTime)
+nahkaFileName = os.path.basename(nahkaFilePath)
+getImageFromLinsee(nahkaFileName, nahkaFilePath)
+
+
+
+if re.search(r'https://', pathToStratix):
+    stratixFileName = getStratixFileNameFromLink(imageMatcherStratix, pathToStratix)
+    getImageFromArtifactory(stratixFileName, pathToStratix)
+else:
+    stratixFilePath = getLastFileFromDir(pathToStratix, imageMatcherStratix, getLastModificationTime)
+    stratixFileName = os.path.basename(stratixFilePath)
+    getImageFromLinsee(stratixFileName, stratixFilePath)
+
+
+
+
+
+extractStratixImage(stratixFileName, pathToTemp)
+
+setNewNahkaFileNameInInstallerScripts(pathToTemp, installerMatcher)
+
+removeOldNahkaImageFile(pathToTempArtifacts, imageMatcherNahka)
+
+copyNewNahkaImageFileToArtifacts(nahkaFileName, pathToTempArtifacts)
+
+createNewStratixImage(pathToTemp, stratixFileNameNew)
+
+removeTempDirectory(pathToTemp)
+
 
 
